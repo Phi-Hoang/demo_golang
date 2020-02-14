6403ea899813a9874abed79f9f501c69708b3a7e
Phi <phi@synopsys.com>
2020-02-14 17:50:40 +0700

    Update findian.go
    Write a program which prompts the user to enter a string. The program searches through the entered string for the characters ‘i’, ‘a’, and ‘n’. The program should print “Found!” if the entered string starts with the character ‘i’, ends with the character ‘n’, and contains the character ‘a’. The program should print “Not Found!” otherwise. The program should not be case-sensitive, so it does not matter if the characters are upper-case or lower-case.

--- hello_world/findian.go
+++ hello_world/findian.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
 	var s string
 	fmt.Print("Please enter a string: ")
-	fmt.Scanf("%s", &s)
-	if strings.Contains(s, "i") && strings.Contains(s, "a") && strings.Contains(s, "n") {
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		s = scanner.Text()
+	}
+	if strings.HasPrefix(strings.ToLower(s), "i") && strings.Contains(strings.ToLower(s), "a") && strings.HasSuffix(strings.ToLower(s), "n") {
 		fmt.Print("Found!")
 	} else {
 		fmt.Print("Not Found!")
